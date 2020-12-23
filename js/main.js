2e21621671d3815065d95bdb76c5c142112ec1fd
Alexandra <Alexandra-guess@users.noreply.github.com>
2020-12-23 18:56:41 +0800

    npm added

--- js/main.js
+++ js/main.js
@@ -170,35 +170,22 @@ class MenuCard {
         this.parent.append(element);
      }
 }
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        23,
-        '.menu .container',
+    const getResource =async (url)=>{
+        const res= await fetch(url);
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    ).render();
-
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню Премиум"',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        45,
-        '.menu .container',
-        
-
-    ).render();
-
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        32,
-        '.menu .container',
-    ).render();
+        return await  res.json();
+};
+    getResource('http://localhost:3000/menu')
+        .then(data=>{
+            data.forEach((
+                {img, altimg, title, descr, price}) => 
+                {new MenuCard(img, altimg, title, descr, price, '.menu .container')
+                .render();
+            });
+        });
 
     //Forms
 
@@ -215,6 +202,19 @@ class MenuCard {
 
     });
 
+    const fetchPostData =async (url, data)=>{
+        const res= await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: data
+        });
+
+        return await  res.json();
+    };
+
+
     function postData(form){
         form.addEventListener('submit', (e)=>{
             e.preventDefault();
@@ -224,35 +224,38 @@ class MenuCard {
             statusMessage.style.cssText=`
             display: block;
             margin: 0 auto;
-
             `;
-            // form.append(statusMessage);
-            form.insertAdjacentElement('afterend', statusMessage);
-
-            let request =new XMLHttpRequest();
-            request.open('POST','server.php');
             
-            request.setRequestHeader('Content-type', 'application/json');
-            let formData=new FormData(form);
+            form.insertAdjacentElement('afterend', statusMessage);
+            
+            const formData=new FormData(form);
 
-            let obj={};
-            formData.forEach(function(value, key){
-                obj[key]=value;
-            });
+            const json =JSON.stringify(Object.fromEntries(formData.entries()));
 
-            const json=JSON.stringify(obj);
 
-            request.send(json);
+            // const obj={};
+            // formData.forEach(function(value, key){
+            //     obj[key]=value;
+            // });
 
-            request.addEventListener('load', ()=>{
-                if(request.status===200){
-                    console.log(request.response);
+            // fetch('server.php',{         //Старый код с использованием fetch вместо функции fetchPostData
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-type': 'application/json'
+            //     },
+            //     body: JSON.stringify(obj)
+            // })
+
+            fetchPostData('http://localhost:3000/requests', json )
+
+            .then(data =>{
+                    console.log(data);
                     showThanks(message.success);
-                    form.reset();
-                        statusMessage.remove();
-                }else{
+                    statusMessage.remove();
+            }).catch(()=>{
                     showThanks(message.failure);
-                }
+            }).finally(()=>{
+                    form.reset();
             });
             
         });
@@ -281,7 +284,10 @@ class MenuCard {
             CloseModalWindow();
 
         },4000);
-
-
     }
+    
+    fetch('http://localhost:3000/menu')
+    .then(data=>data.json())
+    .then(res=>console.log(res));
+
 });
